64506c40bfe4864ceedf12418dc31df37b8c784a
andreshenao14 <162204220+andreshenao14@users.noreply.github.com>
2026-03-18 20:25:23 -0500

    Add files via upload

---
+++ miercoles18.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd 
+import streamlit as st
+import numpy as numpy
+import matplotlib.pyplot as plt
+import seaborn as sns
+st.set_page_config(layout="centered",
+                   page_title="Talento*Tech",
+                   page_icon=":bar_chart:")
+
+t1, t2 =st.columns([0.3,0.7])
+t1.image("imagen.jpeg", width=200)
+t2.title("Análisis de datos con Python")
+t2.markdown( '**tel:** 3127377513 **| email:** andreshenao50@gmail.com' )
+
+steps=st.tabs(["Pestaña 1","Pestaña 2","Pestaña $\sqrt{9}$"])
+
+with steps[0]:
+    st.write("Contenido de la pestaña 1")
+
+with steps[1]:
+    st.write("Pestaña 2")
+
+with steps[2]:
+    df=pd.DataFrame({
+        "A": [1,2,3],
+        "B": [4,5,6],
+        "C": [7,8,9]})
+    st.dataframe(df)
+
+    fig, ex = plt.subplots()
+    ex=sns.barplot(x=["A","B","C"],y=[1,2,3],ax=ex)
+    st.pyplot(fig)
+
+
+
+
